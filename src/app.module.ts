f89f384b8b84a12c67b81e4f407f3cca97754aa6
Erik Garay <angara06.24q@gmail.com>
2026-04-30 23:07:56 -0600

    Configuro base de datos dinámica para Railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { InventarioModule } from './inventario/inventario.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'practica_nest',
+      // Railway
+      host: process.env.MYSQLHOST || 'localhost',
+      port: parseInt(process.env.MYSQLPORT, 10) || 3306,
+      username: process.env.MYSQLUSER || 'root',
+      password: process.env.MYSQLPASSWORD || '',
+      database: process.env.MYSQLDATABASE || 'practica_nest',
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: true, 
     }),
     UsersModule,
     AuthModule,
-    InventarioModule,
   ],
   controllers: [],
   providers: [],
